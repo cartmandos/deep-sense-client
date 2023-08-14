88df999624dc2400bbe0c53011710a3fd36fa380
Nir Cohen <nir.cohen.a30@gmail.com>
2023-08-14 17:00:24 +0300

    added react-router-dom, basic routes, page files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import Analysis from 'pages/Analysis'
+import LandingPage from 'pages/LandingPage'
+import ReportForm from 'pages/ReportForm'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: '/report',
+    element: <ReportForm />,
+  },
+  {
+    path: '/analysis',
+    element: <Analysis />,
+  },
+])
+
 const App = () => {
-  return <div></div>
+  return <RouterProvider router={router} />
 }
 
 export default App
