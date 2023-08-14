449c6b4a23d17512691758d7c0f5f3ce67c80265
Nir Cohen <nir.cohen.a30@gmail.com>
2023-08-14 18:54:19 +0300

    added applayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import AppLayout from './ui/AppLayout'
 import Analysis from './pages/Analysis'
 import LandingPage from './pages/LandingPage'
 import ReportForm from './pages/ReportForm'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LandingPage />,
-  },
-  {
-    path: '/report',
-    element: <ReportForm />,
-  },
-  {
-    path: '/analysis',
-    element: <Analysis />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: '/report',
+        element: <ReportForm />,
+      },
+      {
+        path: '/analysis',
+        element: <Analysis />,
+      },
+    ],
   },
 ])
 
