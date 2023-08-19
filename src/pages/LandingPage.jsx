e0fcb7b3179bd5375f518a9b7ab77320e01c701c
Damien Shomrai <damien.shomrai@pm.me>
2023-08-20 00:54:45 +0300

    bugfix: child element idx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,15 +2,15 @@ import content from '../assets/data/landingPageContent'
 import InfoTab from './InfoTab'
 
 const LandingPage = () => {
-  const renderContent = content.map((item) => {
-    return <InfoTab title={item.title} description={item.description} key={item} />
+  const renderContent = content.map((item, i) => {
+    return <InfoTab title={item.title} description={item.description} key={i} />
   })
 
   return (
     <main className="flex justify-around">
       <div className="flex w-4/5 flex-col">
         <h1 className="m-2  p-4 text-center text-2xl font-medium text-stone-700 ">
-          Dive Sense Annonymoss Incident Reporting and Analysis
+          Dive Sense Anonymous Incident Reporting and Analysis
         </h1>
         {renderContent}
       </div>
