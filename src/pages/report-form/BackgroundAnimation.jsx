4846a34c415e3fd967e2c2ad1fc92294def20f69
Nir Cohen <nir.cohen.a30@gmail.com>
2023-08-23 18:35:18 +0300

    basic animation and bubble svg

--- src/pages/report-form/BackgroundAnimation.jsx
+++ src/pages/report-form/BackgroundAnimation.jsx
@@ -1,5 +1,29 @@
+import OxygenSVG from './OxygenSVG';
+
+import './backgroundAnimation.css';
+
 const BackgroundAnimation = () => {
-  return <div className="absolute h-full w-full "></div>;
+  return (
+    <div className="oxy-lg absolute  h-full w-full overflow-hidden">
+      <div className="oxy-sm relative  flex justify-around">
+        <OxygenSVG initSize={13} />
+        <OxygenSVG initSize={9} />
+        <OxygenSVG initSize={12} />
+        <OxygenSVG initSize={15} />
+        <OxygenSVG initSize={9} />
+        <OxygenSVG initSize={5} />
+        <OxygenSVG initSize={8} />
+        <OxygenSVG initSize={10} />
+        <OxygenSVG initSize={17} />
+        <OxygenSVG initSize={9} />
+        <OxygenSVG initSize={11} />
+        <OxygenSVG initSize={5} />
+        <OxygenSVG initSize={15} />
+        <OxygenSVG initSize={8} />
+        <OxygenSVG initSize={10} />
+      </div>
+    </div>
+  );
 };
 
 export default BackgroundAnimation;
