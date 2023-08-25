0fa1eadf44fdbcc9fb9fa24f1e8db8362ee7a261
Damien Shomrai <damien.shomrai@pm.me>
2023-08-25 17:47:46 +0300

    add absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,12 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@ui', replacement: '/src/ui' },
+      { find: '@lib', replacement: '/src/lib' },
+      { find: '@assets', replacement: '/src/assets' },
+    ],
+  },
+});
