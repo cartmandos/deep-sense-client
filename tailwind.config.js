dd9f035a726a307c76d58bf6989e34d8446d125b
Damien Shomrai <damien.shomrai@pm.me>
2023-08-27 22:47:03 +0300

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
@@ -15,5 +17,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
