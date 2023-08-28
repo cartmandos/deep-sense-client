24a849314dd89db63975df249acaf2db10e52f08
Nir Cohen <nir.cohen.a30@gmail.com>
2023-08-28 16:04:47 +0300

    added form context

--- src/pages/report-form/ReportForm.jsx
+++ src/pages/report-form/ReportForm.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
 import ReportCard from './ReportCard';
+import { FormProvider } from './FormContext';
 
 const ReportForm = () => {
   const [step, setStep] = useState(0);
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <ReportCard step={step} setStep={setStep} />
+      <FormProvider>
+        <ReportCard step={step} setStep={setStep} />
+      </FormProvider>
     </div>
   );
 };
--- src/pages/report-form/Steps/IncidentInfo.jsx
+++ src/pages/report-form/Steps/IncidentInfo.jsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react';
+import { FormProvider } from '../FormContext';
+
 const IncidentInfo = () => {
+  const { formData, handleChange } = useContext(FormProvider);
   return (
     <div>
       <div className="form-control">
         <span className=" mx-3 uppercase">Dive description</span>
-        <textarea className="textarea textarea-lg  border-main-red-dark mx-3 mb-4"></textarea>
+        <textarea
+          className="textarea textarea-lg  border-main-red-dark mx-3 mb-4"
+          name="diveDescription"
+          value={formData.diveDescription}
+          onChange={handleChange}
+        ></textarea>
         <span className=" mx-3 uppercase">incident description</span>
         <textarea className="textarea textarea-lg  border-main-red-dark mx-3 mb-4"></textarea>
         <span className=" mx-3 uppercase">outcome</span>
